48cb320745356d9575f3359f1c4792e84507a3d6
Gr1mmz <mixail.jazz@yandex.ru>
2022-03-02 14:01:56 +0800

    add stock images for movies without images

--- src/components/UI/Movie/Movie.js
+++ src/components/UI/Movie/Movie.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import classes from "./Movie.module.css";
 import {IMG_URL} from "../../../API/API";
+import posterImg from "../../../images/poster.png";
+import backdropImg from "../../../images/backdrop.jpg";
 
 const Movie = ({type, id, title, name, vote_average, poster_path, backdrop_path}) => {
+
+    const poster = poster_path ? `${IMG_URL}${poster_path}` : posterImg;
+    const backdrop = backdrop_path ? `${IMG_URL}${backdrop_path}` : backdropImg;
+
     switch (type) {
         case "poster":
             return (
                 <div className={classes.movie}>
-                    <img src={IMG_URL + poster_path} alt={title}/>
+                    <img src={poster} alt={title}/>
                     <h4 className={classes.header}>{title}</h4>
                     <h4 className={classes.rating}>&#9733; {vote_average}</h4>
                 </div>
@@ -15,7 +21,7 @@ const Movie = ({type, id, title, name, vote_average, poster_path, backdrop_path}
         case "backdrop":
             return (
                 <div className={`${classes.movie} ${classes.backdrop}`}>
-                    <img src={IMG_URL + backdrop_path} alt={title || name}/>
+                    <img src={backdrop} alt={title || name}/>
                     <h4 className={classes.header}>{title || name}</h4>
                     <h4 className={classes.rating}>&#9733; {vote_average}</h4>
                 </div>
